e0cc600a97e67db7e9202f39461c6501cd318b55
tufawayz <vale.tufa.vt@gmail.com>
2025-07-27 18:36:39 +0200

    Delete chrono-break directory

--- chrono-break/src/components/AdBanner.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function AdBanner() {
-  return <div className="h-12 bg-gray-800 flex items-center justify-center text-sm">Ad placeholder</div>;
-}
--- chrono-break/src/screens/CardRewardScreen.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default () => <div className="text-white p-4">CardRewardScreen – coming soon</div>;
